d561eacc9791145842a5ae22d05f3db036585d5f
daniel.p <daniel.p@compie.co.il>
2020-03-23 18:05:04 +0200

    Small fixes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/my-weather-app'));
 
 app.listen(process.env.PORT || 8080);
 
 //PathLocationStrategy
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/my-weather-app/index.html'));
 })
 
 console.log('Console listening!');
